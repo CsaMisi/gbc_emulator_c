79dcf0f7680dbd11b4aeb38c4b53966484475416
buffMidget <misicsanko@gmail.com>
2025-03-10 21:50:37 +0100

    files made, cpu.h extended with macros, and bitmasking

--- include/cpu.h
+++ include/cpu.h
@@ -3,6 +3,13 @@
 
 #include "./common.h"
 
+//Flag bits
+#define FLAG_Z (1<<7);
+#define FLAG_N (1 << 6);
+#define FLAG_H (1 << 5);
+#define FLAG_C (1 << 4);
+
+
 typedef struct CPU{
     /*16 bit registers
     union for access via 8 bit registers*/
@@ -38,9 +45,25 @@ typedef struct CPU{
 
 } CPU_T;
 
-//Prototype Functions
+//Prototype FunctionsS
 void cpu_init(struct CPU *self);
 void cpu_step(struct CPU *self);
 void cpu_interrupt(struct CPU *self, BYTE interrupt_flag);
+
+#define IS_ZEROFLAG_SET(cpu) ((cpu)->F & FLAG_Z)
+#define ZEROFLAG_SET(cpu) ((cpu)->F |= FLAG_Z)
+#define CLEAR_ZEROFLAG(cpu) ((cpu)->F &= ~FLAG_Z)
+
+#define IS_CARRYFLAG_SET(cpu) ((cpu)->F & FLAG_CY)
+#define CARRYFLAG_SET(cpu) ((cpu)->F |= FLAG_CY)
+#define CLEAR_CARRYFLAG(cpu) ((cpu)->F &= ~FLAG_CY)
+
+#define IS_HALFCARRYFLAG_SET(cpu) ((cpu)->F & FLAG_H)
+#define SET_HALFCARRYFLAG(cpu) ((cpu)->F |= FLAG_H)
+#define CLEAR_HALFCARRYFLAG(cpu) ((cpu)->F &= ~FLAG_H)
+
+#define IS_SUBTRACTFLAG_SET(cpu) ((cpu)->F & FLAG_N)
+#define SET_SUBTRACTFLAG(cpu)    ((cpu)->F |= FLAG_N)
+#define CLEAR_SUBTRACT_FLAG(cpu)  ((cpu)->F &= ~FLAG_N)
 
 #endif //CPU_H
