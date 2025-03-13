46bc3c88a0c8bdc577af82ef0833261b574b9778
buffMidget <misicsanko@gmail.com>
2025-03-13 15:12:06 +0100

    refractored the address checking to be a static inline function in common Will put it back in memory.h if theres no use for it elsewhere

--- include/common.h
+++ include/common.h
@@ -2,6 +2,7 @@
 #define COMMON
 #include "stdint.h"
 #include <stdint.h>
+#include "stdbool.h"
 
 enum MemoryMap{
     ROM_START = 0x0000,
@@ -67,5 +68,14 @@ typedef union{
     };
 }SIGNED_WORD;
 
+
+
+static inline bool IsValidAddress(WORD addr){
+    if ((addr.WORD >= ECHO_RAM_START && addr.WORD <= ECHO_RAM_END) ||
+        (addr.WORD >= UNUSED_START && addr.WORD <= UNUSED_END)) {
+        return false;
+    }
+    return true;
+}
 
 #endif 
