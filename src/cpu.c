f342f70ac5f30b281a9c70b66c5634aa799d0455
buffMidget <misicsanko@gmail.com>
2025-04-04 16:48:15 +0200

    started on cpu implementation

--- src/cpu.c
+++ src/cpu.c
@@ -1,1 +1,20 @@
 #include "../include/cpu.h"
+#include <stdint.h>
+
+void cpu_init(CPU_T *self){
+    self->AF.WORD = 0x01B0;
+    self->BC.WORD = 0x0013;
+    self->DE.WORD = 0x00D8;
+    self->HL.WORD = 0x014D;
+    self->SP.WORD = 0xFFFE;
+    self->PC.WORD = 0x0100;  // Program starts at 0x0100
+    
+    self->IME = 1;      // Interrupts enabled
+    self->halted = 0;
+    self->stopped = 0;
+    self->cycles_left = 0;
+}
+
+void cpu_step(CPU_T *self){
+    
+}
